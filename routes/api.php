9d3fba9174ca5a237521093d568ae1c243c9ce76
Vitor <vitorpereira@profkenny.com.br>
2025-04-16 14:56:54 -0300

    Habilitado register temporariamente

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::post('login', [AuthController::class, 'login']); 
+Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('tasks', [TaskController::class, 'index']);
