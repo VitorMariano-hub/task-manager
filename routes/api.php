99a0829640ee4226eec9a3debaef97b846834bcc
Vitor Mariano Pereira <69535241+VitorMariano-hub@users.noreply.github.com>
2025-09-10 16:34:46 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::post('login', [AuthController::class, 'login']); 
 
@@ -12,4 +14,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('tasks', [TaskController::class, 'store']);
     Route::put('tasks/{task}', [TaskController::class, 'update']);
     Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
+});
+
+Route::post('create-temp-user', function() {
+    if (!User::where('email', 'vitor@email.com')->exists()) {
+        User::create([
+            'name' => 'Vitor Mariano',
+            'email' => 'vitor@email.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+
+    return response()->json(['message' => 'Usuário criado']);
 });
